1e46df884d51c65ca9fb5f3ede50f3e631e7132a
Noval Sofyan Ramadhan <novalsofyan1@gmail.com>
2021-01-29 06:14:01 +0700

    Update v0.1.1

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const line = document.querySelector('.line');
+const lineBefore = document.querySelector('.line', '::before');
+const lineAfter = document.querySelector('.line', '::after');
 
 hamburger.addEventListener('click', () => {
     navlinks.classList.toggle('open')
+    line.classList.toggle('open')
+    lineBefore.classList.toggle('open')
+    lineAfter.classList.toggle('open')
     links.forEach(link => {
         link.classList.toggle('fade');
     })
+
 });
